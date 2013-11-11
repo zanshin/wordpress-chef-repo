e676c287c0d683484f338749bc6e70fd331ce810
Mark Nichols <mark@zanshin.net>
2013-11-11 12:47:39 -0600

    Setup Wordpress configuration file.
    Added template for wp-config.php, and made changes to recipe to use same.

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -53,3 +53,31 @@ execute 'untar-wordpress' do
   creates node['phpapp']['path'] + '/wp-settings.php'
 end
 
+wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
+
+remote_file wp_secrets do
+  source 'https://api.wordpress.org/secret_key/1.1/salt'
+  action :create_if_missing
+  mode 0644
+end
+
+salt_data = ''
+
+ruby_block 'fetch-salt-data' do
+  block do
+    salt_data = File.read(wp_secrets)
+  end
+  action :create
+end
+
+template node['phpapp']['path'] + '/wp-config.php' do
+  source 'wp-config.php.erb'
+  mode 0755
+  owner 'root'
+  group 'root'
+  variables(
+    :database   => node['phpapp']['database'],
+    :user       => node['phpapp']['db_username'],
+    :password   => node['phpapp']['db_password'],
+    :wp_secrets => salt_data)
+end
