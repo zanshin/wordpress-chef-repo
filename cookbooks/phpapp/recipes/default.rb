202a477e9606d56623f99247c794537590a28e69
Mark Nichols <mark@zanshin.net>
2013-11-11 13:05:01 -0600

    Set up Apache Virtual Host for WP install.
    Added apache configuration file and set it up for WordPress.

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -16,7 +16,7 @@ include_recipe 'apache2::mod_php5'
 include_recipe 'mysql::ruby'
 
 apache_site "default" do
-  enable true
+  enable false 
 end
 
 mysql_database node['phpapp']['database'] do
@@ -80,4 +80,10 @@ template node['phpapp']['path'] + '/wp-config.php' do
     :user       => node['phpapp']['db_username'],
     :password   => node['phpapp']['db_password'],
     :wp_secrets => salt_data)
+end
+
+web_app 'phpapp' do
+  template 'site.conf.erb'
+  docroot node['phpapp']['path']
+  server_name node['phpapp']['server_name']
 end
