21b6bd5176139899654bbf3c4f2757a306b57a76
Mark Nichols <mark@zanshin.net>
2013-11-11 11:33:52 -0600

    corrected typo in recipe

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -11,7 +11,7 @@ include_recipe 'apache2'
 include_recipe 'mysql::client'
 include_recipe 'mysql::server'
 include_recipe 'php'
-incldue_recipe 'php::module_mysql'
+include_recipe 'php::module_mysql'
 include_recipe 'apache2::mod_php5'
 
 apache_site "default" do
