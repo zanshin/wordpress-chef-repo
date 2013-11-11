e65bdceeb08673f4e1d632be1a9884059eccd197
Mark Nichols <mark@zanshin.net>
2013-11-11 11:45:21 -0600

    Added database cookbook and created database.
    Added the database cookbook (along with its dependencies) and create a step in the default recipe for phpapp to create a database.

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -13,7 +13,13 @@ include_recipe 'mysql::server'
 include_recipe 'php'
 include_recipe 'php::module_mysql'
 include_recipe 'apache2::mod_php5'
+include_recipe 'mysql::ruby'
 
 apache_site "default" do
   enable true
+end
+
+mysql_database node['phpapp']['database'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  action :create
 end
